5e9f4a8cdadc7f225b5df964e7e2e0150eef3176
Matteias Collet <matteias.collet@bluewin.ch>
2017-11-08 18:36:56 +0100

    add fast 79 for ntsc-u

--- src/asm/Fast_79.asm
+++ src/asm/Fast_79.asm
@@ -15,329 +15,326 @@ checkNextStage:
 lis r3, 0x8057
 ori r3, r3, 0x8988
 lhz r4, 2(r3) #Gelato
-andi. r5, r4, 0x4 
-beq+ 0x28
+andi. r5, r4, 0x4
+beq+ 0x2C
 lbz r4, 0xC(r3)
-andi. r5, r4, 0x40 
+andi. r5, r4, 0x40
+lhz r4, 2(r3)
 beq+ 0xC
-li r4, 0x3400 
+li r4, 0x3400
 b loadStage
-andi. r5, r4, 0x4000 
+andi. r5, r4, 0x4000
 beq+ 0xC
-li r4, 0x402 
-b loadStage
-andi. r5, r4, 0x1 
-beq+ 0xC 
-li r4, 0x3400 
+li r4, 0x402
 b loadStage
 li r6, 0
 ori r6, r6, 0x8009
-and r5, r6, r4 
+and r5, r6, r4
 cmpw r5, r6
-bne+ 0x1C 
-andi. r5, r4, 0x4000 
-beq+ 0xC 
-li r4, 0x406 
+bne+ 0x1C
+andi. r5, r4, 0x4000
+beq+ 0xC
+li r4, 0x406
 b loadStage
-li r4, 0x402 
+li r4, 0x402
 b loadStage
 li r6, 0
 ori r6, r6, 0x9
-and r5, r6, r4 
+and r5, r6, r4
 cmpw r5, r6
-bne+ 0xC 
-li r4, 0x403 
+bne+ 0xC
+li r4, 0x403
 b loadStage
 andi. r5, r4, 0x2
-beq+ 0xC 
-li r4, 0x404 
-b loadStage
-andi. r5, r4, 0x8 
-beq+ 0xC 
-li r4, 0x405 
-b loadStage
-andi. r5, r4, 0x4000 
 beq+ 0xC
-li r4, 0x403 
+li r4, 0x404
 b loadStage
-andi. r5, r4, 0x2000 
-beq+ 0xC 
-li r4, 0x402 
+andi. r5, r4, 0x8
+beq+ 0xC
+li r4, 0x405
+b loadStage
+andi. r5, r4, 0x4000
+beq+ 0xC
+li r4, 0x403
+b loadStage
+andi. r5, r4, 0x2000
+beq+ 0xC
+li r4, 0x402
 b loadStage
 li r6, 0
 ori r6, r6, 0x1030
-and r5, r6, r4 
+and r5, r6, r4
 cmpw r5, r6
-bne+ 0xC 
-li r4, 0x401 
+bne+ 0xC
+li r4, 0x401
 b loadStage
-lhz r4, 6(r3) 
-andi. r5, r4, 0x2 
+lhz r4, 6(r3)
+andi. r5, r4, 0x2
 beq+ 0x20
 lbz r4, 0xD(r3)
-andi. r5, r4, 0x2 
-beq+ 0xC 
-li r4, 0x400 
+andi. r5, r4, 0x2
+beq+ 0xC
+li r4, 0x400
 b loadStage
-li r4, 0x901 
+li r4, 0x901
 b loadStage
-andi. r5, r4, 0x1 
-beq+ 0xC 
-li r4, 0x907 
+andi. r5, r4, 0x1
+beq+ 0xC
+li r4, 0x907
 b loadStage
 li r6, 0
 ori r6, r6, 0x8004
-and r5, r6, r4 
-cmpw r5, r6 
-bne+ 0xC 
-li r4, 0x906 
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x906
 b loadStage
 andi. r5, r4, 0x4000
 beq+ 0xC
-li r4, 0x905 
-b loadStage
-andi. r5, r4, 0x2000 
-beq+ 0xC 
-li r4, 0x904 
-b loadStage
-andi. r5, r4, 0x1000
-beq+ 0xC 
-li r4, 0x903 
-b loadStage
-li r6, 0
-ori r6, r6, 0x808
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC 
-li r4, 0x902 
-b loadStage
-andi. r5, r4, 0x400 
-beq+ 0xC 
-li r4, 0x901 
-b loadStage
-lhz r4, 5(r3) 
-andi. r5, r4, 0x8000 
-beq+ 0x20
-lbz r4, 0xD(r3)
-andi. r5, r4, 0x1 
-beq+ 0xC 
-li r4, 0x900 
-b loadStage
-li r4, 0x603 
-b loadStage
-andi. r5, r4, 0x4000 
-beq+ 0xC 
-li r4, 0x607 
+li r4, 0x905
 b loadStage
 andi. r5, r4, 0x2000
-beq+ 0xC 
-li r4, 0x606 
-b loadStage
-andi. r5, r4, 0x1000
-beq+ 0xC 
-li r4, 0x605 
-b loadStage
-li r6, 0
-ori r6, r6, 0x802
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC 
-li r4, 0x604 
-b loadStage
-andi. r5, r4, 0x400 
-beq+ 0xC 
-li r4, 0x603 
-b loadStage
-li r6, 0
-ori r6, r6, 0x201
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC 
-li r4, 0x602 
-b loadStage
-andi. r5, r4, 0x100 
-beq+ 0xC 
-li r4, 0x601 
-b loadStage
-lhz r4, 0(r3) 
-andi. r5, r4, 0x8000 
-beq+ 0x20
-lbz r4, 0xC(r3)
-andi. r5, r4, 0x10 
-beq+ 0xC 
-li r4, 0x600 
-b loadStage
-li r4, 0x205 
-b loadStage
-andi. r5, r4, 0x4000 
-beq+ 0xC 
-li r4, 0x207 
-b loadStage
-li r6, 0
-ori r6, r6, 0x2002
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC
-li r4, 0x206 
-b loadStage
-andi. r5, r4, 0x1000
-beq+ 0xC 
-li r4, 0x205 
-b loadStage
-andi. r5, r4, 0x800 
 beq+ 0xC
-li r4, 0x204 
-b loadStage
-lhz r4, 1(r3) 
-andi. r5, r4, 0x2 
-beq+ 0x20
-lbz r4, 0xC(r3)
-andi. r5, r4, 0x20 
-beq+ 0xC 
-li r4, 0x203 
-b loadStage
-li r4, 0x305 
-b loadStage
-andi. r5, r4, 0x1
-beq+ 0xC 
-li r4, 0x307 
-b loadStage
-andi. r5, r4, 0x8000 
-beq+ 0xC 
-li r4, 0x306 
-b loadStage
-andi. r5, r4, 0x4000
-beq+ 0xC 
-li r4, 0x305 
-b loadStage
-li r6, 0
-ori r6, r6, 0x2004
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC
-li r4, 0x304 
+li r4, 0x904
 b loadStage
 andi. r5, r4, 0x1000
-beq+ 0xC 
-li r4, 0x303 
+beq+ 0xC
+li r4, 0x903
 b loadStage
 li r6, 0
 ori r6, r6, 0x808
-and r5, r6, r4 
+and r5, r6, r4
 cmpw r5, r6
-bne+ 0xC 
-li r4, 0x302 
+bne+ 0xC
+li r4, 0x902
 b loadStage
 andi. r5, r4, 0x400
 beq+ 0xC
-li r4, 0x301 
+li r4, 0x901
 b loadStage
-lhz r4, 3(r3) 
-andi. r5, r4, 0x20 
+lhz r4, 5(r3)
+andi. r5, r4, 0x8000
 beq+ 0x20
-lbz r4, 0xC(r3)
-andi. r5, r4, 0x80 
-beq+ 0xC 
-li r4, 0x300 
-b loadStage
-li r4, 0x504 
-b loadStage
-andi. r5, r4, 0x10
-beq+ 0xC
-li r4, 0x507 
-b loadStage
-li r6, 0
-ori r6, r6, 0x88
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC
-li r4, 0x506 
-b loadStage
-andi. r5, r4, 0x4 
-beq+ 0xC 
-li r4, 0x505 
-b loadStage
-andi. r5, r4, 0x2
-beq+ 0xC
-li r4, 0x504 
-b loadStage
+lbz r4, 0xD(r3)
 andi. r5, r4, 0x1
 beq+ 0xC
-li r4, 0x503 
+li r4, 0x900
 b loadStage
-li r6, 0
-ori r6, r6, 0x8040
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC
-li r4, 0x502 
+li r4, 0x603
 b loadStage
 andi. r5, r4, 0x4000
 beq+ 0xC
-li r4, 0x501 
-b loadStage
-lhz r4, 7(r3) 
-li r6, 0
-ori r6, r6, 0x28
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0x20
-lbz r4, 0xD(r3)
-andi. r5, r4, 0x4 
-beq+ 0xC
-li r4, 0x500 
-b loadStage
-li r4, 0x804 
-b loadStage
-andi. r5, r4, 0x4
-beq+ 0xC
-li r4, 0x807 
+li r4, 0x607
 b loadStage
 andi. r5, r4, 0x2000
 beq+ 0xC
-li r4, 0x806 
-b loadStage
-li r6, 0
-ori r6, r6, 0x11
-and r5, r6, r4 
-cmpw r5, r6
-bne+ 0xC
-li r4, 0x805 
-b loadStage
-andi. r5, r4, 0x8000
-beq+ 0xC
-li r4, 0x804 
-b loadStage
-andi. r5, r4, 0x4000
-beq+ 0xC
-li r4, 0x803 
-b loadStage
-andi. r5, r4, 0x2
-beq+ 0xC
-li r4, 0x802 
+li r4, 0x606
 b loadStage
 andi. r5, r4, 0x1000
 beq+ 0xC
-li r4, 0x801 
+li r4, 0x605
 b loadStage
-lhz r4, 0(r3) 
 li r6, 0
-ori r6, r6, 0x401
-and r5, r6, r4 
+ori r6, r6, 0x802
+and r5, r6, r4
 cmpw r5, r6
 bne+ 0xC
-li r4, 0x800 
+li r4, 0x604
 b loadStage
-andi. r5, r4, 0x300
-beq+ 0xC 
-li r4, 0x202 
+andi. r5, r4, 0x400
+beq+ 0xC
+li r4, 0x603
+b loadStage
+li r6, 0
+ori r6, r6, 0x201
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x602
 b loadStage
 andi. r5, r4, 0x100
 beq+ 0xC
-li r4, 0x201 
+li r4, 0x601
 b loadStage
-li r4, 0x200 
-b loadStage 
+lhz r4, 0(r3)
+andi. r5, r4, 0x8000
+beq+ 0x20
+lbz r4, 0xC(r3)
+andi. r5, r4, 0x10
+beq+ 0xC
+li r4, 0x600
+b loadStage
+li r4, 0x205
+b loadStage
+andi. r5, r4, 0x4000
+beq+ 0xC
+li r4, 0x207
+b loadStage
+li r6, 0
+ori r6, r6, 0x2002
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x206
+b loadStage
+andi. r5, r4, 0x1000
+beq+ 0xC
+li r4, 0x205
+b loadStage
+andi. r5, r4, 0x800
+beq+ 0xC
+li r4, 0x204
+b loadStage
+lhz r4, 1(r3)
+andi. r5, r4, 0x2
+beq+ 0x20
+lbz r4, 0xC(r3)
+andi. r5, r4, 0x20
+beq+ 0xC
+li r4, 0x203
+b loadStage
+li r4, 0x305
+b loadStage
+andi. r5, r4, 0x1
+beq+ 0xC
+li r4, 0x307
+b loadStage
+andi. r5, r4, 0x8000
+beq+ 0xC
+li r4, 0x306
+b loadStage
+andi. r5, r4, 0x4000
+beq+ 0xC
+li r4, 0x305
+b loadStage
+li r6, 0
+ori r6, r6, 0x2004
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x304
+b loadStage
+andi. r5, r4, 0x1000
+beq+ 0xC
+li r4, 0x303
+b loadStage
+li r6, 0
+ori r6, r6, 0x808
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x302
+b loadStage
+andi. r5, r4, 0x400
+beq+ 0xC
+li r4, 0x301
+b loadStage
+lhz r4, 3(r3)
+andi. r5, r4, 0x20
+beq+ 0x20
+lbz r4, 0xC(r3)
+andi. r5, r4, 0x80
+beq+ 0xC
+li r4, 0x300
+b loadStage
+li r4, 0x504
+b loadStage
+andi. r5, r4, 0x10
+beq+ 0xC
+li r4, 0x507
+b loadStage
+li r6, 0
+ori r6, r6, 0x88
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x506
+b loadStage
+andi. r5, r4, 0x4
+beq+ 0xC
+li r4, 0x505
+b loadStage
+andi. r5, r4, 0x2
+beq+ 0xC
+li r4, 0x504
+b loadStage
+andi. r5, r4, 0x1
+beq+ 0xC
+li r4, 0x503
+b loadStage
+li r6, 0
+ori r6, r6, 0x8040
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x502
+b loadStage
+andi. r5, r4, 0x4000
+beq+ 0xC
+li r4, 0x501
+b loadStage
+lhz r4, 7(r3)
+li r6, 0
+ori r6, r6, 0x28
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0x20
+lbz r4, 0xD(r3)
+andi. r5, r4, 0x4
+beq+ 0xC
+li r4, 0x500
+b loadStage
+li r4, 0x804
+b loadStage
+andi. r5, r4, 0x4
+beq+ 0xC
+li r4, 0x807
+b loadStage
+andi. r5, r4, 0x2000
+beq+ 0xC
+li r4, 0x806
+b loadStage
+li r6, 0
+ori r6, r6, 0x11
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x805
+b loadStage
+andi. r5, r4, 0x8000
+beq+ 0xC
+li r4, 0x804
+b loadStage
+andi. r5, r4, 0x4000
+beq+ 0xC
+li r4, 0x803
+b loadStage
+andi. r5, r4, 0x2
+beq+ 0xC
+li r4, 0x802
+b loadStage
+andi. r5, r4, 0x1000
+beq+ 0xC
+li r4, 0x801
+b loadStage
+lhz r4, 0(r3)
+li r6, 0
+ori r6, r6, 0x401
+and r5, r6, r4
+cmpw r5, r6
+bne+ 0xC
+li r4, 0x800
+b loadStage
+andi. r5, r4, 0x300
+beq+ 0xC
+li r4, 0x202
+b loadStage
+andi. r5, r4, 0x100
+beq+ 0xC
+li r4, 0x201
+b loadStage
+li r4, 0x200
+b loadStage
 
 loadPinna:
 
