d46c0a0f5de88474653b2e66e2f8552e9b37a02f
QbeRoot <qbe.root@gmail.com>
2018-01-24 20:44:37 +0100

    Updated Fast Any% asm code
    - Added the IGT reset and the episode name fix.
    - All memory addresses are now at the beginning, and only appear once
    each, which makes porting easier.
    - Removed loadPinna, setupPinna now sets r4 directly.
    - Optimized register usage; this version only writes to r0, r3, r4 and r5, all of which are overwritten right after the injected code runs.

--- src/asm/Fast_Any%.asm
+++ src/asm/Fast_Any%.asm
@@ -1,7 +1,14 @@
 @802A6710
 
+lis r3, 0x817F
+ori r3, r3, 0x0100
+li r0, 0
+stw r0, 12(r3)
+li r0, 1
+stb r0, 1(r3)
 lis r3, 0x803E
 ori r3, r3, 0x9710
+lwz r5, -0x6060(r13)
 lbz r4, 2(r3)
 cmpwi r4, 14
 beq- setupCasino
@@ -12,241 +19,230 @@ bgt- done
 
 checkNextStage:
 
-lis r3, 0x8057
-ori r3, r3, 0x8988
-lbz r4, 7(r3)
-andi. r5, r4, 0x1
+lbz r4, 7(r5)
+andi. r0, r4, 0x1
 beq+ 0xC
 li r4, 0x3400
 b loadStage
-lbz r4, 6(r3)
-andi. r5, r4, 0x80
+lbz r4, 6(r5)
+andi. r0, r4, 0x80
 beq+ 0xC
 li r4, 0x906
 b loadStage
-andi. r5, r4, 0x40
+andi. r0, r4, 0x40
 beq+ 0xC
 li r4, 0x905
 b loadStage
-andi. r5, r4, 0x20
+andi. r0, r4, 0x20
 beq+ 0xC
 li r4, 0x904
 b loadStage
-andi. r5, r4, 0x10
+andi. r0, r4, 0x10
 beq+ 0xC
 li r4, 0x903
 b loadStage
-andi. r5, r4, 0x8
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0x902
 b loadStage
-andi. r5, r4, 0x4
+andi. r0, r4, 0x4
 beq+ 0xC
 li r4, 0x901
 b loadStage
-lbz r4, 5(r3)
-andi. r5, r4, 0x40
+lbz r4, 5(r5)
+andi. r0, r4, 0x40
 beq+ 0xC
 li r4, 0x900
 b loadStage
-andi. r5, r4, 0x20
+andi. r0, r4, 0x20
 beq+ 0xC
 li r4, 0x606
 b loadStage
-andi. r5, r4, 0x10
+andi. r0, r4, 0x10
 beq+ 0xC
 li r4, 0x605
 b loadStage
-andi. r5, r4, 0x8
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0x604
 b loadStage
-andi. r5, r4, 0x4
+andi. r0, r4, 0x4
 beq+ 0xC
 li r4, 0x603
 b loadStage
-andi. r5, r4, 0x2
+andi. r0, r4, 0x2
 beq+ 0xC
 li r4, 0x602
 b loadStage
-andi. r5, r4, 0x1
+andi. r0, r4, 0x1
 beq+ 0xC
 li r4, 0x601
 b loadStage
-lbz r4, 0(r3)
-andi. r5, r4, 0x40
+lbz r4, 0(r5)
+andi. r0, r4, 0x40
 beq+ 0xC
 li r4, 0x600
 b loadStage
-andi. r5, r4, 0x20
+andi. r0, r4, 0x20
 beq+ 0xC
 li r4, 0x206
 b loadStage
-lbz r4, 2(r3)
-andi. r5, r4, 0x1
+lbz r4, 2(r5)
+andi. r0, r4, 0x1
 beq+ 0xC
 li r4, 0x205
 b loadStage
-lbz r4, 1(r3)
-andi. r5, r4, 0x80
+lbz r4, 1(r5)
+andi. r0, r4, 0x80
 beq+ 0xC
 li r4, 0x306
 b loadStage
-andi. r5, r4, 0x40
+andi. r0, r4, 0x40
 beq+ 0xC
 li r4, 0x305
 b loadStage
-andi. r5, r4, 0x20
+andi. r0, r4, 0x20
 beq+ 0xC
 li r4, 0x304
 b loadStage
-andi. r5, r4, 0x10
+andi. r0, r4, 0x10
 beq+ 0xC
 li r4, 0x303
 b loadStage
-andi. r5, r4, 0x8
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0x302
 b loadStage
-andi. r5, r4, 0x4
+andi. r0, r4, 0x4
 beq+ 0xC
 li r4, 0x301
 b loadStage
-lbz r4, 4(r3)
-andi. r5, r4, 0x10
+lbz r4, 4(r5)
+andi. r0, r4, 0x10
 beq+ 0xC
 li r4, 0x300
 b loadStage
-andi. r5, r4, 0x8
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0x506
 b loadStage
-andi. r5, r4, 0x2
+andi. r0, r4, 0x2
 beq+ 0xC
 li r4, 0x502
 b loadStage
-andi. r5, r4, 0x1
+andi. r0, r4, 0x1
 beq+ 0xC
 li r4, 0x503
 b loadStage
-lbz r4, 3(r3)
-andi. r5, r4, 0x80
+lbz r4, 3(r5)
+andi. r0, r4, 0x80
 beq+ 0xC
 li r4, 0x502
 b loadStage
-andi. r5, r4, 0x40
+andi. r0, r4, 0x40
 beq+ 0xC
 li r4, 0x501
 b loadStage
-lbz r4, 8(r3)
-andi. r5, r4, 0x4
+lbz r4, 8(r5)
+andi. r0, r4, 0x4
 beq+ 0xC
 li r4, 0x500
 b loadStage
-lbz r4, 7(r3)
-andi. r5, r4, 0x80
+lbz r4, 7(r5)
+andi. r0, r4, 0x80
 beq+ 0xC
 li r4, 0x806
 b loadStage
-lbz r4, 8(r3)
-andi. r5, r4, 0x1
+lbz r4, 8(r5)
+andi. r0, r4, 0x1
 beq+ 0xC
 li r4, 0x805
 b loadStage
-lbz r4, 7(r3)
-andi. r5, r4, 0x20
+lbz r4, 7(r5)
+andi. r0, r4, 0x20
 beq+ 0xC
 li r4, 0x804
 b loadStage
-andi. r5, r4, 0x40
+andi. r0, r4, 0x40
 beq+ 0xC
 li r4, 0x803
 b loadStage
-lbz r4, 8(r3)
-andi. r5, r4, 0x2
+lbz r4, 8(r5)
+andi. r0, r4, 0x2
 beq+ 0xC
 li r4, 0x802
 b loadStage
-lbz r4, 7(r3)
-andi. r5, r4, 0x10
+lbz r4, 7(r5)
+andi. r0, r4, 0x10
 beq+ 0xC
 li r4, 0x801
 b loadStage
-lbz r4, 3(r3)
-andi. r5, r4, 0x4
+lbz r4, 3(r5)
+andi. r0, r4, 0x4
 beq+ 0xC
 li r4, 0x800
 b loadStage
-andi. r5, r4, 0x8
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0x406
 b loadStage
-lbz r4, 0(r3)
-andi. r5, r4, 0x10
+lbz r4, 0(r5)
+andi. r0, r4, 0x10
 beq+ 0xC
 li r4, 0x400
 b loadStage
-andi. r5, r4, 0x8
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0x204
 b loadStage
-andi. r5, r4, 0x4
+andi. r0, r4, 0x4
 beq+ 0xC
 li r4, 0x203
 b loadStage
-andi. r5, r4, 0x2
+andi. r0, r4, 0x2
 beq+ 0xC
 li r4, 0x202
 b loadStage
 li r4, 0x200
-b loadStage
-
-loadPinna:
-
-add r4, r8, r9
 
 loadStage:
 
-lis r3, 0x803E
-ori r3, r3, 0x9710
 stw r4, 0(r3)
+stb r4, 0x00DF(r5)
 b done
 
 setupPinna:
 
-li r8,3328
 lwz r4,-4(r3)
-cmpwi r4,1280
+cmpwi r4,0x500
 bne- 0x0C
-li r9,0
-b loadPinna
-cmpwi r4,1282
+li r4,0xD00
+b loadStage
+cmpwi r4,0x502
 bne- 0x0C
-li r9,1
-b loadPinna
-cmpwi r4,1284
+li r4,0xD01
+b loadStage
+cmpwi r4,0x504
 bne- 0x0C
-li r9,2
-b loadPinna
-cmpwi r4,1285
+li r4,0xD02
+b loadStage
+cmpwi r4,0x505
 bne- 0x0C
-li r9,3
-b loadPinna
-cmpwi r4,1286
+li r4,0xD03
+b loadStage
+cmpwi r4,0x506
 bne- 0x0C
-li r9,4
-b loadPinna
-cmpwi r4,1287
+li r4,0xD04
+b loadStage
+cmpwi r4,0x507
 bne+ done
-li r9,5
-b loadPinna
+li r4,0xD05
+b loadStage
 
 setupCasino:
 
-lis r3, 0x8057
-ori r3, r3, 0x8988
-lbz r4, 5(r3)
-andi. r5, r4, 0x8
+lbz r4, 5(r5)
+andi. r0, r4, 0x8
 beq+ 0xC
 li r4, 0xE01
 b loadStage
